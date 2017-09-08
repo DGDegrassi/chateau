7a55568652c25a9e8e5429e5e1da66332e4378d1
Daniel <ddegrassi17@gmail.com>
2017-09-08 11:34:28 +0000

    set up amazons3, carrierwave etc

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,13 @@
 CarrierWave.configure do |config|
-  config.storage = :file
-
+  # config.storage = :file
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+  }
+  config.storage = :fog
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
   # Use S3 if you want
   # config.fog_credentials = {
   #   provider:               'AWS',
