1a340f187b68e02b583fbf9859855b75da6bf10a
Daniel <ddegrassi17@gmail.com>
2017-09-11 09:45:24 +0000

    tinkering with successful email redirect

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,8 +7,8 @@ class BookingsController < ApplicationController
 		@booking = Booking.new(params[:booking])
 		@booking.request = request
 		if @booking.deliver
-			flash[:notice] = 'Thank you for your message.  We will contact you soon!'
-			redirect_to root_path
+			flash.now[:notice] = 'Thank you for your message.  We will contact you soon!'
+			render :new
 		else
 			flash.now[:error] = 'Cannot send message.'
 			render :new
