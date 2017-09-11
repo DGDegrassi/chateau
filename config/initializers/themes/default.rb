9ad47dd0c98d1559be9684a392588edfafdd219d
Daniel <ddegrassi17@gmail.com>
2017-09-11 08:31:40 +0000

    created press page template

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -56,6 +56,18 @@
     title: 'Photo Link 7',
     partable_type: 'Spina::Photo'
   }, {
+    name:           'textforphoto7',
+    title:          'Text for Photo 7',
+    partable_type:  'Spina::Text'
+  }, {
+    name: 'photolink8',
+    title: 'Photo Link 8',
+    partable_type: 'Spina::Photo'
+  }, {
+    name:           'textforphoto8',
+    title:          'Text for Photo 8',
+    partable_type:  'Spina::Text'
+  }, {
     name: 'photolink1',
     title: 'Photo Link 1',
     partable_type: 'Spina::Photo'
@@ -79,7 +91,7 @@
     name:           'textforphoto3',
     title:          'Text for Photo 3',
     partable_type:  'Spina::Text'
-  },]
+  }]
 
   theme.view_templates = [{
     name:       'homepage',
@@ -91,16 +103,17 @@
     description: 'A page with a large picture across the top, the title and intro text, a single box for text and 3 pictures as links along the bottom',
     page_parts: ['intro', 'photo', 'text', 'photolink1', 'textforphoto1', 'photolink2', 'textforphoto2', 'photolink3', 'textforphoto3']
   }, {
-    name: 'picturewith2text',
-    title: 'Picture with 2 texts',
-    description: 'A page with a large picture across the top two text boxes, with the right one larger then the left',
-    page_parts: ['text', 'text2', 'photo']
-  }, {
-    name: 'textonly',
-    title: 'Text Only',
-    description: 'A page with one text box',
-    page_parts: ['text']
-  }, {
+  #  {
+  #   name: 'picturewith2text',
+  #   title: 'Picture with 2 texts',
+  #   description: 'A page with a large picture across the top two text boxes, with the right one larger then the left',
+  #   page_parts: ['text', 'text2', 'photo']
+  # }, {
+  #   name: 'textonly',
+  #   title: 'Text Only',
+  #   description: 'A page with one text box',
+  #   page_parts: ['text']
+  # },
     name: 'photocollection',
     title: 'Photo Album',
     description: 'A page with a title and photo album',
@@ -110,7 +123,12 @@
       title: 'Photos on Left, Text on Right',
       description: 'A page with a photo collection on the left and text on the right',
       page_parts: ['intro', 'text', 'photo_collection', 'photolink1', 'textforphoto1', 'photolink2', 'textforphoto2', 'photolink3', 'textforphoto3']
-    }]  
+    }, {
+      name: 'presspage',
+      title: 'Press Page',
+      description: 'The press page, with photos on the left lined up with text on the right',
+      page_parts: ['intro', 'photo', 'photolink1', 'textforphoto1', 'photolink2', 'textforphoto2', 'photolink3', 'textforphoto3', 'photolink4', 'textforphoto4', 'photolink5', 'textforphoto5', 'photolink6', 'textforphoto6', 'photolink7', 'textforphoto7', 'photolink8', 'textforphoto8']
+      }]  
 
   theme.custom_pages = [{
     name:           'homepage',
