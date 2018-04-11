c65b2b38a0a439e94b41df72ff691b2367be7712
Daniel <ddegrassi17@gmail.com>
2018-04-11 09:33:07 +0000

    fixing vulnerability from iframe fix

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Chateau
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOWALL'
+    'X-Frame-Options' => 'ALLOW-FROM http://chateaudemissery.com/'
 		}
   end
 end
