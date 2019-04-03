7e4b89cdbfed38c1a17fc73c405bbb0009fa516a
Daniel <ddegrassi17@gmail.com>
2019-04-03 18:48:17 +0000

    privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	match '/booking',     to: 'bookings#new',             via: 'get'
 	resources 'bookings', only: [:new, :create]
+	match '/privacypolicy',     to: 'privacy_policy#index',             via: 'get'
+	resources 'privacypolicy'
   mount Spina::Engine => '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
